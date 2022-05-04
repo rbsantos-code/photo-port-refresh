e78e00100955489fd578929e04f2f59a6c5f1b01
rbsantos-code <richardbriansantos91@yahoo.com>
2022-05-04 16:55:43 -0700

    setup gallery component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div>
       <Nav />
       <main>
+        <Gallery />
         <About />
       </main>
     </div>
---
+++ src/components/Gallery/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Gallery() {
+    return (
+        <section>
+            <h1>Name</h1>
+            <p>Description</p>
+        </section>
+    );        
+}
+
+
+export default Gallery;
