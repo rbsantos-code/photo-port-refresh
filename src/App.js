facb98b611298dc1cb313b9f9f40a4587764b874
rbsantos-code <richardbriansantos91@yahoo.com>
2022-05-04 18:54:27 -0700

    fix typo

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         currentCategory={currentCategory}
       />
       <main>
-        <Gallery />
+        <Gallery currentCategory={currentCategory}/>
         <About />
       </main>
     </div>
