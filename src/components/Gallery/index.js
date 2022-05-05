c6676b36f537792ae6e1ab75f00d67f4c2f079d7
rbsantos-code <richardbriansantos91@yahoo.com>
2022-05-04 17:18:33 -0700

    render cat name and image

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from '../../assets/small/commercial/0.jpg';
 
-function Gallery() {
+function Gallery(props) {
+    const currentCategory = {
+        name: 'commercial',
+        description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    };
+
     return (
         <section>
-            <h1>Name</h1>
-            <p>Description</p>
+            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <p>{currentCategory.name}</p>
+            <div>
+                <img
+                    src={photo}
+                />
+            </div>
         </section>
     );        
 }
